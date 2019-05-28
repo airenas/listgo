a017cb652056baf77a52d5e07de6b62b8b1587ac
airenas <airenass@gmail.com>
2019-05-28 09:36:55 +0300

    Get rid of convey

--- internal/app/result/service_test.go
+++ internal/app/result/service_test.go
@@ -8,10 +8,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"bitbucket.org/airenas/listgo/internal/pkg/test/mocks"
 	"github.com/gorilla/mux"
 	"github.com/petergtz/pegomock"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 var audioFileLoaderMock *mocks.MockFileLoader
@@ -27,60 +28,34 @@ func initTest() {
 }
 
 func TestWrongPath(t *testing.T) {
-
-	Convey("Given a HTTP request for /invalid", t, func() {
-		req := httptest.NewRequest("GET", "/invalid", nil)
-		resp := httptest.NewRecorder()
-
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	req := httptest.NewRequest("GET", "/invalid", nil)
+	resp := httptest.NewRecorder()
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func TestNoID(t *testing.T) {
-	Convey("Given a HTTP request for /audio/", t, func() {
-		req := httptest.NewRequest("GET", "/audio/", nil)
-		resp := httptest.NewRecorder()
-
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	req := httptest.NewRequest("GET", "/audio/", nil)
+	resp := httptest.NewRecorder()
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func TestGET(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request for /audio", t, func() {
-		req := httptest.NewRequest("GET", "/audio/id", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
-		pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
-		pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, nil)
-		pegomock.When(fileMock.Seek(pegomock.AnyInt64(), pegomock.AnyInt())).ThenReturn(int64(2), nil)
-		pegomock.When(fileMock.Read(anyByteArray())).Then(
-			func(params []pegomock.Param) pegomock.ReturnValues {
-				return []pegomock.ReturnValue{2, nil}
-			})
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 200", func() {
-				So(resp.Code, ShouldEqual, 200)
-			})
-			Convey("Then the response body should not be empty", func() {
-				So(resp.Body.String(), ShouldNotBeEmpty)
-			})
+	req := httptest.NewRequest("GET", "/audio/id", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
+	pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
+	pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, nil)
+	pegomock.When(fileMock.Seek(pegomock.AnyInt64(), pegomock.AnyInt())).ThenReturn(int64(2), nil)
+	pegomock.When(fileMock.Read(anyByteArray())).Then(
+		func(params []pegomock.Param) pegomock.ReturnValues {
+			return []pegomock.ReturnValue{2, nil}
 		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 200)
+	assert.NotEmpty(t, resp.Body.String())
 }
 
 func newRouter() *mux.Router {
@@ -91,155 +66,95 @@ func newRouter() *mux.Router {
 
 func Test_FileNameProviderFails(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request", t, func() {
-		req := httptest.NewRequest("GET", "/audio/id", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("", errors.New("Can not get"))
+	req := httptest.NewRequest("GET", "/audio/id", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("", errors.New("Can not get"))
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func Test_FileLoaderFails(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request", t, func() {
-		req := httptest.NewRequest("GET", "/audio/id", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
-		pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(nil, errors.New("Can not get"))
+	req := httptest.NewRequest("GET", "/audio/id", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
+	pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(nil, errors.New("Can not get"))
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func Test_FileStatFails(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request", t, func() {
-		req := httptest.NewRequest("GET", "/audio/id", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
-		pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
-		pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, errors.New("Can not get"))
+	req := httptest.NewRequest("GET", "/audio/id", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(fileNameProviderMock.Get(pegomock.AnyString())).ThenReturn("olia", nil)
+	pegomock.When(audioFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
+	pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, errors.New("Can not get"))
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func TestResultNoID(t *testing.T) {
-	Convey("Given a HTTP request for /result/", t, func() {
-		req := httptest.NewRequest("GET", "/result/", nil)
-		resp := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/result/", nil)
+	resp := httptest.NewRecorder()
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func TestResultNoFile(t *testing.T) {
-	Convey("Given a HTTP request for /result/id/", t, func() {
-		req := httptest.NewRequest("GET", "/result/id/", nil)
-		resp := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/result/id/", nil)
+	resp := httptest.NewRecorder()
+	newRouter().ServeHTTP(resp, req)
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	assert.Equal(t, resp.Code, 404)
 }
+
 func TestResultWrongFile(t *testing.T) {
-	Convey("Given a HTTP request for /result/id/..file", t, func() {
-		req := httptest.NewRequest("GET", "/result/id/..file", nil)
-		resp := httptest.NewRecorder()
-
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 400", func() {
-				So(resp.Code, ShouldEqual, 400)
-			})
-		})
-	})
+	req := httptest.NewRequest("GET", "/result/id/..file", nil)
+	resp := httptest.NewRecorder()
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 400)
 }
 
 func TestResultGET(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request for /result/id/file", t, func() {
-		req := httptest.NewRequest("GET", "/result/id/file", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
-		pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, nil)
-		pegomock.When(fileMock.Seek(pegomock.AnyInt64(), pegomock.AnyInt())).ThenReturn(int64(2), nil)
-		pegomock.When(fileMock.Read(anyByteArray())).Then(
-			func(params []pegomock.Param) pegomock.ReturnValues {
-				return []pegomock.ReturnValue{2, nil}
-			})
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-
-			Convey("Then the response should be a 200", func() {
-				So(resp.Code, ShouldEqual, 200)
-			})
-			Convey("Then the response body should not be empty", func() {
-				So(resp.Body.String(), ShouldNotBeEmpty)
-			})
+	req := httptest.NewRequest("GET", "/result/id/file", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
+	pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, nil)
+	pegomock.When(fileMock.Seek(pegomock.AnyInt64(), pegomock.AnyInt())).ThenReturn(int64(2), nil)
+	pegomock.When(fileMock.Read(anyByteArray())).Then(
+		func(params []pegomock.Param) pegomock.ReturnValues {
+			return []pegomock.ReturnValue{2, nil}
 		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 200)
+	assert.NotEmpty(t, resp.Body.String())
 }
 
 func TestResult_FileLoaderFails(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request", t, func() {
-		req := httptest.NewRequest("GET", "/result/id/file", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(nil, errors.New("Can not get"))
+	req := httptest.NewRequest("GET", "/result/id/file", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(nil, errors.New("Can not get"))
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 func TestResult_FileStatFails(t *testing.T) {
 	initTest()
-	Convey("Given a HTTP request", t, func() {
-		req := httptest.NewRequest("GET", "/result/id/file", nil)
-		resp := httptest.NewRecorder()
-		pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
-		pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, errors.New("Can not get"))
+	req := httptest.NewRequest("GET", "/result/id/file", nil)
+	resp := httptest.NewRecorder()
+	pegomock.When(resultFileLoaderMock.Load(pegomock.AnyString())).ThenReturn(fileMock, nil)
+	pegomock.When(fileMock.Stat()).ThenReturn(mockedFileInfo{}, errors.New("Can not get"))
 
-		Convey("When the request is handled by the Router", func() {
-			newRouter().ServeHTTP(resp, req)
-			Convey("Then the response should be a 404", func() {
-				So(resp.Code, ShouldEqual, 404)
-			})
-		})
-	})
+	newRouter().ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 404)
 }
 
 type mockedFileInfo struct {
