761909bae95cd2d860ce7c8bdc202d2c7d61d30d
airenas <airenass@gmail.com>
2019-08-22 12:44:53 +0300

    Add TF proto buffer generation script

---
+++ build/prepareTFProto.sh
@@ -0,0 +1,24 @@
+git clone -b r1.7 https://github.com/tensorflow/serving.git
+git clone -b r1.7 https://github.com/tensorflow/tensorflow.git
+
+mkdir -p gen
+PROTOC_OPTS='-I tensorflow -I serving --go_out=plugins=grpc:gen'
+
+eval "protoc $PROTOC_OPTS serving/tensorflow_serving/apis/*.proto"
+eval "protoc $PROTOC_OPTS serving/tensorflow_serving/config/*.proto"
+eval "protoc $PROTOC_OPTS serving/tensorflow_serving/util/*.proto"
+eval "protoc $PROTOC_OPTS serving/tensorflow_serving/sources/storage_path/*.proto"
+eval "protoc $PROTOC_OPTS tensorflow/tensorflow/core/framework/*.proto"
+eval "protoc $PROTOC_OPTS tensorflow/tensorflow/core/example/*.proto"
+eval "protoc $PROTOC_OPTS tensorflow/tensorflow/core/lib/core/*.proto"
+eval "protoc $PROTOC_OPTS tensorflow/tensorflow/core/protobuf/{saver,meta_graph}.proto"
+
+IMP="bitbucket.org\/airenas\/listgo\/internal\/pkg\/tensorflow"
+find gen -name '*.go' | xargs sed -i "s/\"tensorflow/\"$IMP/g"
+
+mv gen/tensorflow ../internal/pkg
+mv gen/tensorflow_serving ../internal/pkg
+
+# rm - r gen
+# rm - r tensorflow
+# rm - r serving
--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module bitbucket.org/airenas/listgo
 go 1.12
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad
 	github.com/cenkalti/backoff v2.1.1+incompatible
 	github.com/confluentinc/confluent-kafka-go v1.0.0
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/gogap/env_json v0.0.0-20150503135429-86150085ddbe // indirect
 	github.com/gogap/env_strings v0.0.1 // indirect
+	github.com/golang/protobuf v1.3.1
 	github.com/google/uuid v1.1.0
 	github.com/gorilla/mux v1.7.0
 	github.com/gorilla/websocket v1.4.0
@@ -29,6 +29,6 @@ require (
 	github.com/spf13/viper v1.3.1
 	github.com/streadway/amqp v0.0.0-20190225234609-30f8ed68076e
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+	google.golang.org/grpc v1.23.0
 	gopkg.in/yaml.v2 v2.2.2
 )
