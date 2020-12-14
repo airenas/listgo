a8b9564d8b1eab112d5911aba103da16be321282
airenas <airenass@gmail.com>
2020-12-14 12:32:37 +0200

    Add custom login auth for smtp protocol

--- internal/app/inform/emailSender.go
+++ internal/app/inform/emailSender.go
@@ -2,8 +2,10 @@ package inform
 
 import (
 	"net/smtp"
+	"strings"
 	"time"
 
+	"bitbucket.org/airenas/listgo/internal/app/inform/auth"
 	"bitbucket.org/airenas/listgo/internal/pkg/cmdapp"
 	"github.com/jordan-wright/email"
 )
@@ -16,12 +18,21 @@ type SimpleEmailSender struct {
 func newSimpleEmailSender() (*SimpleEmailSender, error) {
 	r := SimpleEmailSender{}
 	var err error
-	r.sendPool, err = email.NewPool(cmdapp.Config.GetString("smtp.host")+":"+cmdapp.Config.GetString("smtp.port"), 1,
-		smtp.PlainAuth("", cmdapp.Config.GetString("smtp.username"), cmdapp.Config.GetString("smtp.password"), cmdapp.Config.GetString("smtp.host")))
+	r.sendPool, err = email.NewPool(cmdapp.Config.GetString("smtp.host")+":"+cmdapp.Config.GetString("smtp.port"), 1, newAuth())
 	if err != nil {
 		return nil, err
 	}
 	return &r, nil
+}
+
+func newAuth() smtp.Auth {
+	if strings.ToLower(cmdapp.Config.GetString("smtp.useLogin")) == "true" {
+		cmdapp.Log.Infof("Using custom login auth")
+		return auth.LoginAuth(cmdapp.Config.GetString("smtp.username"), cmdapp.Config.GetString("smtp.password"))
+	}
+	cmdapp.Log.Infof("Using plain login auth ")
+	return smtp.PlainAuth("", cmdapp.Config.GetString("smtp.username"), cmdapp.Config.GetString("smtp.password"),
+		cmdapp.Config.GetString("smtp.host"))
 }
 
 //Send sends email
