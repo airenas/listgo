bc70cd083bdb81528fc2eeb8e7b8bc4ccd39471c
airenas <airenass@gmail.com>
2022-05-04 11:41:12 +0300

    Add scripts for building manager

--- cmd/managerService/main.go
+++ cmd/managerService/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"bitbucket.org/airenas/listgo/internal/app/manager"
+	"github.com/labstack/gommon/color"
 )
 
 func main() {
+	printBanner()
 	manager.Execute()
+}
+
+var (
+	version string
+)
+
+func printBanner() {
+	banner := `
+    ___      __ 
+   / (_)____/ /_
+  / / / ___/ __/
+ / / (__  ) /_  
+/_/_/____/\__/  
+    __  ___            ___              ____ 
+   /  |/  /___ _____  /   | ____ ____  / __ \
+  / /|_/ / __ `+ "`" +`/ __ \/ /| |/ __ `+ "`" +`/ _ \/ /_/ /
+ / /  / / /_/ / / / / ___ / /_/ /  __/ _, _/ 
+/_/  /_/\__,_/_/ /_/_/  |_\__, /\___/_/ |_|  v: %s
+					  /____/ 
+%s
+________________________________________________________                                                 
+
+`
+	cl := color.New()
+	cl.Printf(banner, cl.Red(version), cl.Green("bitbucket.org/airenas/listgo"))
 }
