8fce879d70e8ef81a7bd47fe4662b5f50afaab0c
airenas <airenass@gmail.com>
2022-05-04 12:29:57 +0300

    Add scripts for worker service

--- cmd/cmdWorkerService/main.go
+++ cmd/cmdWorkerService/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "bitbucket.org/airenas/listgo/internal/app/cmdworker"
+import (
+	"bitbucket.org/airenas/listgo/internal/app/cmdworker"
+	"github.com/labstack/gommon/color"
+)
 
 func main() {
+	printBanner()
 	cmdworker.Execute()
+}
+
+var (
+	version string
+)
+
+func printBanner() {
+	banner := `
+    ___      __ 
+   / (_)____/ /_
+  / / / ___/ __/
+ / / (__  ) /_  
+/_/_/____/\__/  
+                
+              ____             __ __             
+ _      __   / __ \   _____   / //_/   ___  _____
+| | /| / /  / / / /  / ___/  / ,<     / _ \/ ___/
+| |/ |/ /  / /_/ /  / /     / /| |   /  __/ /    
+|__/|__/   \____/  /_/     /_/ |_|   \___/_/    v: %s
+
+
+%s
+________________________________________________________                                                 
+
+`
+	cl := color.New()
+	cl.Printf(banner, cl.Red(version), cl.Green("bitbucket.org/airenas/listgo"))
 }
