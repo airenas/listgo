ec2943e4d2991e580e25c8932f713575f5e3bfe5
airenas <airenass@gmail.com>
2022-05-05 10:13:44 +0300

    Add zoom banner

--- cmd/zoomService/main.go
+++ cmd/zoomService/main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"bitbucket.org/airenas/listgo/internal/app/zoom"
+	"github.com/labstack/gommon/color"
 )
 
 func main() {
+	printBanner()
 	zoom.Execute()
 }
+
+var (
+	version string
+)
+
+func printBanner() {
+	banner := `
+    ___      __ 
+   / (_)____/ /_
+  / / / ___/ __/
+ / / (__  ) /_  
+/_/_/____/\__/  
+                          ____             
+ ____           ____     / __ \   ____ ___ 
+/_  /          / __ \   / / / /  / __ `+ "`" +`__ \
+ / /_    _    / /_/ /  / /_/ /  / / / / / /
+/___/   (_)   \____/   \____/  /_/ /_/ /_/   v: %s
+
+%s
+________________________________________________________                                                 
+
+`
+	cl := color.New()
+	cl.Printf(banner, cl.Red(version), cl.Green("bitbucket.org/airenas/listgo"))
+}
+
