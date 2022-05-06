3f6985e06036c7ea95a26d343dd66209d0d5087c
airenas <airenass@gmail.com>
2022-05-06 13:58:49 +0300

    Add status build scripts

--- cmd/statusProviderService/main.go
+++ cmd/statusProviderService/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "bitbucket.org/airenas/listgo/internal/app/status"
+import (
+	"bitbucket.org/airenas/listgo/internal/app/status"
+	"github.com/labstack/gommon/color"
+)
 
 func main() {
+	printBanner()
 	status.Execute()
+}
+
+var version string
+
+func printBanner() {
+	banner := `
+    ___      __ 
+   / (_)____/ /_
+  / / / ___/ __/
+ / / (__  ) /_  
+/_/_/____/\__/  
+         __  ___  ______          
+   _____/ /_/   |/_  __/_  _______
+  / ___/ __/ /| | / / / / / / ___/
+ (__  ) /_/ ___ |/ / / /_/ (__  ) 
+/____/\__/_/  |_/_/  \__,_/____/  | v: %s
+
+%s
+________________________________________________________                                                 
+
+`
+	cl := color.New()
+	cl.Printf(banner, cl.Red(version), cl.Green("bitbucket.org/airenas/listgo"))
 }
